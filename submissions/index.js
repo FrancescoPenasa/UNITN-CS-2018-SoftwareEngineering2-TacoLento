54d834d815dccabc87460afe0f193d02f588491c
daniscal <43404840+daniscal@users.noreply.github.com>
2018-11-28 10:37:03 +0100

    first implem.

--- submissions/index.js
+++ submissions/index.js
@@ -1,1 +1,29 @@
 
+module.exports.set = function(app){
+
+ /**
+  * Submission structure
+  */
+
+var submissions = [{
+  id : 1,
+  date : "27/11/2018",
+  userId : 1,
+  examId : 1,
+  answer: [1,"test"]
+},{
+  id : 2,
+  date : "28/11/2018",
+  userId : 2,
+  examId : 1,
+  answer: [1,"test"]
+}];
+  
+  
+  
+  
+  
+  
+  
+  
+}
