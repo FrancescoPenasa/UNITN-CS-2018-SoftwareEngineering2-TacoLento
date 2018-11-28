8d3c368feea583439acf0bdf904f2d41c658fb59
daniscal <43404840+daniscal@users.noreply.github.com>
2018-11-28 11:06:05 +0100

    update submissions get and put

--- submissions/index.js
+++ submissions/index.js
@@ -26,21 +26,51 @@ var submissions = [{
  */
 
 app.get('/submissions/', function (req, res) {
+ if(submissions.lenght==0)
+ {
+  res.status(400)
+  res.send("No submissions found")
+ }
+ else
+ {
   res.status(200);
   res.send(submissions);
+ }
 });
 
 app.post('/submissions/', function (req, res) {
   var submission = req.body;
+  var error = 0;
   submission.id = submissions.lenght + 1;
   submission.date = req.body.date;
   submission.userId = req.body.userId;
   submission.examId = req.body.examId;
   submission.answers = [req.body.idTask , req.body.answer];
-  submissions.push(submission);
-  res.location("/submissions/" + submissions.id);
-  res.status(201);
-  res.send();
+  
+  if(submissions.date == "")
+  {
+    res.send(400);
+  }
+  if(submissions.userId == null)
+  {
+    res.send(400);
+  }
+  if(submissions.examId == null)
+  {
+    res.send(400);
+  }
+  if(submissions.answer.idTask == null)
+  {
+    res.send(400);
+  }
+ 
+  if (error != 0) 
+  {
+    submissions.push(submission);
+    res.location("/submissions/" + submissions.id);
+    res.status(201);
+    res.send();
+  }
 });
  
 }
