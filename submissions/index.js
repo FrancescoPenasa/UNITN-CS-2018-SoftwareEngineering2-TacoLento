3f9ff4d78471e2ad10ede9df6ca3504613a5ea10
daniscal <43404840+daniscal@users.noreply.github.com>
2018-11-28 10:40:12 +0100

    add verbs

--- submissions/index.js
+++ submissions/index.js
@@ -18,12 +18,29 @@ var submissions = [{
   examId : 1,
   answer: [1,"test"]
 }];
-  
-  
-  
-  
-  
-  
-  
-  
+
+ /**
+
+ * /SUBMISSIONS/ Verbs
+
+ */
+
+app.get('/submissions/', function (req, res) {
+  res.status(200);
+  res.send(submissions);
+});
+
+app.post('/submissions/', function (req, res) {
+  var submission = req.body;
+  submission.id = submissions.lenght + 1;
+  submission.date = req.body.date;
+  submission.userId = req.body.userId;
+  submission.examId = req.body.examId;
+  submission.answers = [req.body.idTask , req.body.answer];
+  submissions.push(submission);
+  res.location("/submissions/" + submissions.id);
+  res.status(201);
+  res.send();
+});
+ 
 }
