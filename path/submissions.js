601a7143ddba2d403185588a8e62c0406d0d6817
Francesco_Penasa_W <francesco.penasa-1@studenti.unitn.it>
2018-12-01 20:25:17 +0100

    update tasks and submissions as it was before but refactored

--- path/submissions.js
+++ path/submissions.js
@@ -6,34 +6,79 @@ submissions.use(bodyParser.json());
 submissions.use(bodyParser.urlencoded({ extended: true }));
 
 /*** DATABASE ***/
-var submissions_db = [{}];
+var submissions_db = [{
+  id : 1,
+  date : "27/11/2018",
+  userId : 1,
+  examId : 1,
+  answer: [1,"test"]
+},{
+  id : 2,
+  date : "28/11/2018",
+  userId : 2,
+  examId : 1,
+  answer: [1,"test"]
+}];
 
 
 /*** FUNCTIONS ***/
 
 
-/*** METHODS ***/
 /**
-* GET /submissions
-* ...
-*/
-submissions.get('/', async (req, res) => {
-  //TODO
-  // res.status(200);
-  // return res.json(submissions_db);
+ * /SUBMISSIONS/ Verbs
+ */
+
+app.get('/submissions/', function (req, res) {
+ if(submissions_db.lenght==0)
+ {
+  res.status(400)
+  res.send("No submissions found")
+ }
+ else
+ {
+  res.status(200);
+  res.send(submissions_db);
+ }
 });
 
-/**
-* POST /submissions
-* ...
-*/
-submissions.post('/', async (req, res) => {
-  //TODO
-  // res.location('/'+submissions.id);
-  // res.status(201);
-  // return res.send();
-});
+app.post('/submissions/', function (req, res) {
+  var submission = req.body;
+  var error = 0;
+  submission.id = submissions_db.lenght + 1;
+  submission.date = req.body.date;
+  submission.userId = req.body.userId;
+  submission.examId = req.body.examId;
+  submission.answers = [req.body.idTask , req.body.answer];
 
+  if(submissions_db.date == "")
+  {
+    res.status(400);
+    error = 1;
+  }
+  if(submissions_db.userId == null)
+  {
+    res.status(400);
+    error = 1;
+  }
+  if(submissions_db.examId == null)
+  {
+    res.status(400);
+    error = 1;
+  }
+  if(submissions_db.answer.idTask == null)
+  {
+    res.status(400);
+    error = 1;
+  }
+
+  if (error != 0)
+  {
+    submissions_db.push(submission);
+    res.location("/submissions/" + submission.id);
+    res.status(201);
+    res.send();
+  }
+});
 
 module.exports = {
 	submissions: submissions
--- path/tasks.test.js
+++ path/tasks.test.js
@@ -47,3 +47,77 @@ var app = require('../index');
 //   .expect(201)
 // 	done();
 // });
+
+
+
+
+const fetch = require("node-fetch");
+
+const url = "http://localhost:3000/tasks"
+//const url = 'https://tacolento-develop.herokuapp.com/'
+
+ // TASKS.test.js
+
+ ///// GET /users /////
+test('GET /tasks/ if empty should return 404 ', async () => {
+ const response = await request(app).get('/tasks/');
+  expect (response.status).toBe(404);
+  expect (response.text).toBe("No tasks found");
+});
+
+
+test('POST /tasks/ should return 201', () => {
+  return fetch(url, {
+    method: 'POST',
+        body: JSON.stringify({
+          "taskType": "MC",
+          "question": "ABC?",
+          "subject" : "Math"}),
+        headers: {
+        'Content-Type': 'application/json',
+        },
+      })
+      .then(r => expect(r.status).toEqual(201));
+});
+
+test('POST /tasks/ should return 400 because task is wrong', () => {
+  return fetch(url, {
+    method: 'POST',
+        body: JSON.stringify({
+          "taskType": "aa",
+          "question": "ABC?",
+          "subject" : "Math"}),
+        headers: {
+        'Content-Type': 'application/json',
+        },
+      })
+      .then(r => expect(r.status).toEqual(400))
+});
+
+test('POST /tasks/ should return 400 becuse question is empty', () => {
+  return fetch(url, {
+    method: 'POST',
+        body: JSON.stringify({
+          "taskType": "OP",
+          "question": "",
+          "subject" : "Math"}),
+        headers: {
+        'Content-Type': 'application/json',
+        },
+      })
+      .then(r => expect(r.status).toEqual(400))
+});
+
+test('POST /tasks/ should return 400 because subject is empty', () => {
+  return fetch(url, {
+    method: 'POST',
+        body: JSON.stringify({
+          "taskType": "OP",
+          "question": "ABC?",
+          "subject" : ""}),
+        headers: {
+        'Content-Type': 'application/json',
+        },
+      })
+      .then(r => expect(r.status).toEqual(400))
+});
