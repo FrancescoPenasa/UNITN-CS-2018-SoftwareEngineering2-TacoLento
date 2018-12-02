454188bc04d63ede36c3d0be781f0dd738d9f829
daniscal <43404840+daniscal@users.noreply.github.com>
2018-12-02 21:49:31 +0100

    add function

--- path/submissions.js
+++ path/submissions.js
@@ -22,12 +22,75 @@ var submissions_db = [{
 
 
 /*** FUNCTIONS ***/
+/*** create a new submissions ***/
+function create_submissions(req)
+{
+	return submissions = 
+	{
+		id : (submissions_db.lenght),
+  		date : req.body.date,
+  		userId : req.body.userId,
+  		examId : req.body.examId,
+  		answers : [req.body.idTask , req.body.answer]
+	};
+}
+
+function input_validity(submissions)
+{
+	if (isNaN(submissions.date) || isNaN(submissions.userId) || isNaN(submissions.examId) || isNaN(submissions.answer.idTask))
+		return false
+}
+
+function input_validity(id)
+{
+	if(!(isNaN(id)))
+		return false
+	if(id == null)
+		return false
+	if(id % 1 !== 0)
+		return false
+	if(submissions_db[id] = {})
+		return false
+	return true
+}
+
+function userId_validity(userId)
+{
+	if(!(isNaN(userId)))
+		return false
+	if(userId == null)
+		return false
+	if(userId % 1 !== 0)
+		return false
+	return true
+}
+
+function examId_validity(examId)
+{
+	if(!(isNaN(examId)))
+		return false
+	if(examId == null)
+		return false
+	if(examId % 1 !== 0)
+		return false
+	return true
+}
+
+function delete_submissions(sub_Id)
+{
+ 	let sub = submissions_db.find(x => x.id === submissions_id);
+  	let index = array.indexOf(sub);
+  	submissions_db[index] = {};
+}	
 
 
 /**
  * /SUBMISSIONS/ Verbs
  */
 
+//-----------------------------
+// GET /submissions/
+//-----------------------------
 submissions.get('/submissions/', function (req, res) {
  if(submissions_db.lenght==0)
  {
@@ -41,43 +104,34 @@ submissions.get('/submissions/', function (req, res) {
  }
 });
 
-submissions.post('/submissions/', function (req, res) {
-  var submission = req.body;
-  var error = 0;
-  submission.id = submissions_db.lenght + 1;
-  submission.date = req.body.date;
-  submission.userId = req.body.userId;
-  submission.examId = req.body.examId;
-  submission.answers = [req.body.idTask , req.body.answer];
+//-----------------------------
+// POST /submissions/
+//-----------------------------
 
-  if(submissions_db.date == "")
-  {
-    res.status(400);
-    error = 1;
-  }
-  if(submissions_db.userId == null)
-  {
-    res.status(400);
-    error = 1;
-  }
-  if(submissions_db.examId == null)
-  {
-    res.status(400);
-    error = 1;
-  }
-  if(submissions_db.answer.idTask == null)
-  {
-    res.status(400);
-    error = 1;
-  }
-
-  if (error != 0)
-  {
-    submissions_db.push(submission);
-    res.location("/submissions/" + submission.id);
-    res.status(201);
-    res.send();
-  }
+submissions.post('/submissions/', function (req, res) 
+{
+	let submission = create_submissions(req)
+	if(!(input_validity(submission)))
+  	{
+		res.status(400)
+		return res.send("invalid input")
+	}
+	if(!(userId_validity(submission.userId)))
+	{
+		res.status(400)
+		return res.send("invalid userId")
+	}
+	if(!(examId_validity(submission.examId)))
+	{
+		res.status(400)
+		return res.send("invalid examId")
+	}
+	
+	submissions_db.push(submission);
+    	res.location("/" + submission.id);
+    	res.status(201);
+    	return res.send();
+  	}
 });
 
 module.exports = {
