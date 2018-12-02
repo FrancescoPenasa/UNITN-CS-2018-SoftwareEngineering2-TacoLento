1d61c42a421b6e7c714facea62aa6f9f981a688f
Francesco_Penasa_W <francesco.penasa-1@studenti.unitn.it>
2018-12-02 17:25:31 +0100

    add PUT GET DELETE /users/id implementation

--- path/users.js
+++ path/users.js
@@ -16,7 +16,13 @@ users.use(bodyParser.urlencoded({ extended: true }));
 * password : String
 * }
 */
-var users_db = [{}];
+var users_db = [{
+  id : 0,
+  name:	"admin",
+  surname: "admin",
+  email: "admin@admin.it",
+  password: "admin"
+}];
 
 
 /*** FUNCTIONS ***/
@@ -26,7 +32,7 @@ var users_db = [{}];
 */
 function create_user(req){
   return user = {
-    id : (users_db.length + 1),
+    id : (users_db.length),
     name : req.body.name,
     surname : req.body.surname,
     email : req.body.email,
@@ -37,7 +43,7 @@ function create_user(req){
 /**
 * Input not valid if
 * - name or surname or email is not a String
-* - name or surnam or email is null
+* - name or surname or email is null or empty
 * - password long less than 5 characters
 */
 function input_validity(user){
@@ -53,15 +59,45 @@ function input_validity(user){
 }
 
 /**
+* Id not valid if
+* - id is not a number
+* - id is a float
+* - id is null
+* - id of a deleted user
+*/
+function id_validity(id){
+  if (!(isNaN(id)))
+    return false
+  if (id==null)
+    return false
+  if (id % 1 !== 0)
+    return false
+  if (users_db[id] == {})
+    return false
+  return true
+}
+
+/**
 * Email not valid if
 * - there is already an user with the same email in the db.
 */
-function email_validity(email){
+function email_validity(user_email){
   for (var i = 1; i < users_db.length; i++){
-    if (users_db.email == email)
+    if (users_db.email == user_email)
       return false
     }
   return true
+}
+
+
+/**
+* Email not valid if
+* - there is already an user with the same email in the db.
+*/
+function remove_user(user_id){
+  let user = users_db.find(x => x.id === user_id);
+  let index = array.indexOf(user);
+  users_db[index] = {};
 }
 
 /*** METHODS ***/
@@ -86,14 +122,14 @@ users.get('/', async (req, res) => {
 * If the parameters respect the policy, create a user in the db.
 */
 users.post('/', async (req, res) => {
-  let user = create_user(req);
+  let user = create_user(req)
 
-  if (! input_validity(user)){
+  if (! (input_validity(user))){
     res.status(400)
     return res.send("invalid input")
   }
 
-  if (! email_validity(email)){
+  if (! (email_validity(user.email))){
     res.status(409)
     return res.send("email already exist")
   }
@@ -111,34 +147,60 @@ users.post('/', async (req, res) => {
 users.get('/:id', async (req, res) => {
   let id = req.params.id;
 
-
-  res.status(200);
-  res.json(users_db);
+  if(! (id_validity(id))){
+    res.status(400)
+    return res.send()
+  }
+  if(id >= users_db.length){
+    res.status(404)
+    return res.send()
+  }
+  res.status(302);
+  return res.json(users_db[id]);
 });
 
 /**
 * PUT /users/id
-*
+* 202 400 404
 */
 users.put('/:id', async (req, res) => {
   let id = req.params.id;
+  let user = create_user(req);
+  if(!(id_validity(id)) && !(input_validity(user))){
+    res.status(400)
+    return res.send()
+  }
 
+  if(id >= users_db.length){
+    res.status(404)
+    return res.send()
+  }
 
-  res.status(200);
-  res.json(users_db);
+  users_db[id] = user;
+  res.status(202);
+  res.send();
 });
 
 /**
 * DELETE /users/id
-*
+* 202 400 404
 */
 users.delete('/:id', async (req, res) => {
   let id = req.params.id;
 
-
-  res.status(200);
-  res.json(users_db);
+  if(!(id_validity(id))){
+    res.status(400)
+    return res.send()
+  }
+  if(id >= users_db.length){
+    res.status(404)
+    return res.send()
+  }
+  remove_user(id);
+  res.status(202);
+  res.send();
 });
+
 
 module.exports = {
 	users: users
